0f8d61dc437a0c7622c6baff93f715dd7824748c
jacob <jbutler8@terpmail.umd.edu>
2018-03-13 21:18:51 -0700

    update fetch() function

--- api/managed-records.js
+++ api/managed-records.js
@@ -4,13 +4,18 @@ import URI from "urijs";
 // /records endpoint
 window.path = "http://localhost:3000/records";
 
-function retrieve(options) {
+function retrieve(options = {}) {
   // decompose search parameters from input
   if (!options.page) options.page = 1;
   if (!options.colors) options.colors = [];
   var limit = 10;
 
-
+  fetch(window.path, options)
+    .then(function(response) {
+      response.json();
+    }).then(function(myJson) {
+      console.log(myJson);
+    });
 }
 
 export default retrieve;
