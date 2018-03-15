2ba6c84f7df5b55817c47fe2d058c1df249b6008
jacob <jbutler8@terpmail.umd.edu>
2018-03-15 11:32:00 -0700

    update function call

--- api/managed-records.js
+++ api/managed-records.js
@@ -6,13 +6,37 @@ window.path = "http://localhost:3000/records";
 
 function retrieve(options = {}) {
   // decompose search parameters from input
+  if (!options.offset) options.offset = 0;
   if (!options.page) options.page = 1;
   if (!options.colors) options.colors = [];
-  var limit = 10;
+  options.limit = 10;
   var uri = URI(window.path).search(options);
-  let path = window.path + "?" + uri.query();
-  console.log(path)
-  fetch(path);
+  let query = window.path + "?" + uri.query() ;
+  console.log(query);
+  // fetch(query)
+  //   .then(function(response) {
+  //     return response.json();
+  //   });
+  // }
+  postData(query); // what do i put as the second arg?
+}
+
+function postData(data) {
+  return new Promise(function (resolve, reject) {
+    fetch({
+      path: data.path,
+      method: 'GET',
+      headers: data.headers || {},
+      queryParams: data.queryParams || {},
+      body: JSON.stringify(data) || {}
+    })
+      .then(function(response) {
+        if (response.status !== 200) {
+          reject(new Error('hello'));
+        }
+        resolve(response.json());
+      });
+  });
 }
 
 export default retrieve;
