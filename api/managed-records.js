3c8add7c59526452efbc3b29c7e9335eaaf05457
jacob <jbutler8@terpmail.umd.edu>
2018-03-15 13:20:53 -0700

    update offset with page number

--- api/managed-records.js
+++ api/managed-records.js
@@ -6,11 +6,11 @@ window.path = "http://localhost:3000/records";
 
 function retrieve(options = {}) {
   // decompose search parameters from input
-  if (!options.offset) options.offset = 0;
   if (!options.page) options.page = 1;
   if (!options.colors) options.colors = [];
   options.limit = 10;
 
+  options.offset = (options.page - 1) * options.limit;
   // rewriting the options object to fit the query constraints. The
   // options object is given with a key of 'colors' while the query
   // query needs to be made with 'color[]'
@@ -18,10 +18,14 @@ function retrieve(options = {}) {
   options.colors.forEach(function(color) {
     options["color[]"].push(color);
   });
+
+  // format search parameters;
   delete options.colors;
+  delete options.page;
 
   var uri = URI(window.path).search(options);
   let query = window.path + "?" + uri.query();
+  console.log(query)
 
   // construct an object to house all of the fetch promise constraints.
   var data = {};
@@ -44,13 +48,13 @@ function postData(data) {
       body: JSON.stringify(data) || {}
     })
       .then(function(response) {
+        console.log(response)
         if (response.status !== 200) {
           response.text()
             .then(function(text) {
               reject(text);
             });
         }
-        console.log(response)
         response.json()
           .then(function(response) {
             resolve(response);
