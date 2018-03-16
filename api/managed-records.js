3622729b3b2eb0e6e44ec54b17c9ba24fae5dfa5
jacob <jbutler8@terpmail.umd.edu>
2018-03-15 17:21:27 -0700

    parse payload into functional format

--- api/managed-records.js
+++ api/managed-records.js
@@ -7,6 +7,7 @@ window.path = "http://localhost:3000/records";
 function retrieve(options = {}) {
   // decompose search parameters from input
   if (!options.page) options.page = 1;
+  var page = options.page;          // used to format payload
   if (!options.colors) options.colors = [];
 
   // set the page displayed using the offset query.
@@ -33,14 +34,14 @@ function retrieve(options = {}) {
   var data = {};
   data.path = query;
 
-  return postData(data).then(function(response) {
+  return getData(data).then(function(response) {
     console.log(response);
   }).catch(function(error) {
     console.log(error);
   });
 }
 
-function postData(data) {
+function getData(data) {
   return new Promise(function (resolve, reject) {
     fetch({
       path: data.path,
@@ -49,21 +50,52 @@ function postData(data) {
       queryParams: data.queryParams || {},
       body: JSON.stringify(data) || {}
     })
-      .then(function(response) {
-        console.log(response)
-        if (response.status !== 200) {
-          console.log("Request unsuccessful")
-          response.text()
-            .then(function(text) {
-              reject(text);
-            });
-        }
-        response.json()
-          .then(function(response) {
-            resolve(response);
-          });
+    .then(function(response) {
+      console.log('fetched response', response);
+      if (response.status !== 200) {
+        console.log("Request unsuccessful");
+        response.text()
+        .then(function(text) {
+          console.log('text', text);
+          reject(text);
+        });
+      }
+      response.json()
+      .then(function(finalResponse) {
+        resolve(finalResponse);
       });
+    });
   });
+}
+
+function parsePayload(payload, page) {
+  var data = {
+    ids: [],
+    open: [],
+    closedPrimaryCount: 0
+  };
+
+  // parse page data
+  data.previousPage = (page === 1) ? null : page - 1;
+  data.nextPage = (page === 50) ? null : page + 1;
+
+  var primaryColors = ['red', 'blue', 'yellow'];
+  payload.forEach(function(datum) {
+    // add id to data
+    data.ids.push(datum.id);
+
+    // determine whether or not datum is primary color
+    var primary = primaryColors.includes(datum.color);
+    if (primary) data.closedPrimaryCount++;
+
+    if (datum.disposition === 'open') {
+      // add isPrimary to datum, then add to output
+      datum.isPrimary = primary;
+      data.open.push(datum);
+    }
+  });
+
+  return data;
 }
 
 export default retrieve;
